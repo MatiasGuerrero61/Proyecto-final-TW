223c745e7ac1e6bd4fad726ea2f6fe3538c00fdf
21diego <morinigodiego21@gmail.com>
2020-05-31 01:50:08 -0300

    fixed

--- src/main/java/ar/edu/unlam/tallerweb1/modelo/Mascota.java
+++ src/main/java/ar/edu/unlam/tallerweb1/modelo/Mascota.java
@@ -8,45 +8,43 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Mascota  {
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private long id;
-private String animal;
-private String caracteristica;
-private String nombre;
-@ManyToOne
-private Usuario duenio;
-public long getId() {
-	return id;
-}
-public void setId(long id) {
-	this.id = id;
-}
-public String getAnimal() {
-	return animal;
-}
-public void setAnimal(String animal) {
-	this.animal = animal;
-}
-public String getCaracteristica() {
-	return caracteristica;
-}
-public void setCaracteristica(String caracteristica) {
-	this.caracteristica = caracteristica;
-}
-public String getNombre() {
-	return nombre;
-}
-public void setNombre(String nombre) {
-	this.nombre = nombre;
-}
-public Usuario getDuenio() {
-	return duenio;
-}
-public void setDuenio(Usuario duenio) {
-	this.duenio = duenio;
-}
-
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String animal;
+	private String caracteristica;
+	private String nombre;
+	@ManyToOne
+	private Usuario duenio;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getAnimal() {
+		return animal;
+	}
+	public void setAnimal(String animal) {
+		this.animal = animal;
+	}
+	public String getCaracteristica() {
+		return caracteristica;
+	}
+	public void setCaracteristica(String caracteristica) {
+		this.caracteristica = caracteristica;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Usuario getDuenio() {
+		return duenio;
+	}
+	public void setDuenio(Usuario duenio) {
+		this.duenio = duenio;
+	}
 
 }
