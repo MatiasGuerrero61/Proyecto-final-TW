51469b3b007fdba98083b75e41bfc949c2c76a90
Diego Navarrete <diego.navarrete@redb.ee>
2020-06-09 22:47:03 -0300

    Agrego test que fala

---
+++ src/test/java/ar/edu/unlam/tallerweb1/PruebaTest.java
@@ -0,0 +1,16 @@
+package ar.edu.unlam.tallerweb1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PruebaTest {
+
+
+    @Test
+    public void testQueFalla() {
+
+        Boolean variable = Boolean.FALSE;
+        assertTrue(variable);
+    }
+}
