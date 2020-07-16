606c11714965da48956c1f15383ff3e097995a70
Diego Navarrete <diego.navarrete@redb.ee>
2020-07-15 23:02:09 -0300

    test y fix hibernate context

--- src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioFacturaImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/servicios/ServicioFacturaImpl.java
@@ -1,164 +1,162 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.time.LocalDateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import ar.edu.unlam.tallerweb1.modelo.Carrito;
 import ar.edu.unlam.tallerweb1.modelo.Descuento;
 import ar.edu.unlam.tallerweb1.modelo.EnumEstadoDeCompra;
 import ar.edu.unlam.tallerweb1.modelo.Factura;
 import ar.edu.unlam.tallerweb1.modelo.Item;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioFactura;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service("servicioFactura")
 @Transactional
 public class ServicioFacturaImpl implements ServicioFactura {
-	
-	private RepositorioFactura servicioFacturaDao;
-	private ServicioDescuento servicioDescuento;
-	private ServicioCarrito servicioCarrito;
-	private ServicioItem servicioItem;
-	
-	@Autowired
+
+    private RepositorioFactura servicioFacturaDao;
+    private ServicioDescuento servicioDescuento;
+    private ServicioCarrito servicioCarrito;
+    private ServicioItem servicioItem;
+
+    @Autowired
     public ServicioFacturaImpl(RepositorioFactura servicioFacturaDao,
-    						   ServicioItem servicioItem,
-    						   ServicioCarrito servicioCarrito,
-    						   ServicioDescuento servicioDescuento) {
-		this.servicioFacturaDao = servicioFacturaDao;
+                               ServicioItem servicioItem,
+                               ServicioCarrito servicioCarrito,
+                               ServicioDescuento servicioDescuento) {
+        this.servicioFacturaDao = servicioFacturaDao;
         this.servicioItem = servicioItem;
         this.servicioCarrito = servicioCarrito;
         this.servicioDescuento = servicioDescuento;
     }
-	
-	@Override
-	public boolean cargarCodigoDeDescuento(Factura factura, String codigo) {
-		Descuento descuento = this.servicioDescuento.verificarValidez(codigo);
-		if(descuento != null) {
-			this.servicioDescuento.invalidarDescuento(descuento);
-			factura.setDescuento(descuento);
-			factura.setSubtotalDescuentos(this.servicioDescuento.calcularDescuento(descuento, factura.getSubtotalSinDescuentos()));
-			factura.setImporteFinal(this.servicioDescuento.calcularImporteConDescuento(descuento, factura.getSubtotalSinDescuentos()));
-			this.servicioFacturaDao.actualizarFactura(factura);
-			return true;
-		}
-		return false;
-	}
 
-	@Override
-	public void anularDescuento(Factura factura) {
-		this.servicioFacturaDao.anularDescuento(factura);			
-	}
+    @Override
+    public boolean cargarCodigoDeDescuento(Factura factura, String codigo) {
+        Descuento descuento = this.servicioDescuento.verificarValidez(codigo);
+        if (descuento != null) {
+            this.servicioDescuento.invalidarDescuento(descuento);
+            factura.setDescuento(descuento);
+            factura.setSubtotalDescuentos(this.servicioDescuento.calcularDescuento(descuento, factura.getSubtotalSinDescuentos()));
+            factura.setImporteFinal(this.servicioDescuento.calcularImporteConDescuento(descuento, factura.getSubtotalSinDescuentos()));
+            this.servicioFacturaDao.actualizarFactura(factura);
+            return true;
+        }
+        return false;
+    }
 
-	@Override
-	public Factura generarFactura(Carrito carrito) {
-		Factura factura = new Factura();
-		factura.setCarrito(carrito);
-		factura.setEstado(EnumEstadoDeCompra.SIN_CONFIRMAR);
-		BigDecimal subtotalSinDescuentos = this.servicioItem.sumarImportesDeItems(carrito);
-		factura.setSubtotalSinDescuentos(subtotalSinDescuentos);
-		factura.setSubtotalDescuentos(new BigDecimal("0"));
-		factura.setImporteFinal(subtotalSinDescuentos); 
-		this.servicioItem.fijarPrecioDeCompra(this.servicioItem.listarItems(carrito));
-		factura.setFechaYHoraDeCompra( LocalDateTime.now());
-		this.servicioFacturaDao.guardarFactura(factura);		
-		return factura;
-	}
-	
-	@Override
-	public Factura actualizarImportesFactura(Factura factura) {
-		BigDecimal subtotalSinDescuentos = this.servicioItem.sumarImportesDeItems(factura.getCarrito());
-		factura.setSubtotalSinDescuentos(subtotalSinDescuentos);
-		if(factura.getDescuento() != null) {
-			factura.setSubtotalDescuentos(this.servicioDescuento.calcularDescuento(factura.getDescuento(), factura.getSubtotalSinDescuentos()));
-			factura.setImporteFinal(this.servicioDescuento.calcularImporteConDescuento(factura.getDescuento(), factura.getSubtotalSinDescuentos()));
-		}
-		else {
-			factura.setSubtotalDescuentos(new BigDecimal("0"));
-			factura.setImporteFinal(subtotalSinDescuentos); 			
-		}
-		this.servicioItem.fijarPrecioDeCompra(this.servicioItem.listarItems(factura.getCarrito()));
-		factura.setFechaYHoraDeCompra( LocalDateTime.now());
-		this.servicioFacturaDao.actualizarFactura(factura);
-		return factura;
-	}
+    @Override
+    public void anularDescuento(Factura factura) {
+        this.servicioFacturaDao.anularDescuento(factura);
+    }
 
-	@Override
-	public Factura sincronizarFactura(Long idCarrito) {
-		Carrito carrito = this.servicioCarrito.obtenerCarrito(idCarrito);
-		Factura factura = this.servicioFacturaDao.buscarFacturaSinConfirmar(carrito.getId());
-		if(factura != null) {
-			factura = this.actualizarImportesFactura(factura);
-			return factura;
-		}
-		else {
-			return this.generarFactura(carrito);
-		}
-	}
+    @Override
+    public Factura generarFactura(Carrito carrito) {
+        Factura factura = new Factura();
+        factura.setCarrito(carrito);
+        factura.setEstado(EnumEstadoDeCompra.SIN_CONFIRMAR);
+        BigDecimal subtotalSinDescuentos = this.servicioItem.sumarImportesDeItems(carrito);
+        factura.setSubtotalSinDescuentos(subtotalSinDescuentos);
+        factura.setSubtotalDescuentos(new BigDecimal("0"));
+        factura.setImporteFinal(subtotalSinDescuentos);
+        this.servicioItem.fijarPrecioDeCompra(this.servicioItem.listarItems(carrito));
+        factura.setFechaYHoraDeCompra(LocalDateTime.now());
+        this.servicioFacturaDao.guardarFactura(factura);
+        return factura;
+    }
 
-	@Override
-	public List<Item> listarItems(Factura factura) {
-		return this.servicioItem.listarItems(factura.getCarrito());
-	}
+    @Override
+    public Factura actualizarImportesFactura(Factura factura) {
+        BigDecimal subtotalSinDescuentos = this.servicioItem.sumarImportesDeItems(factura.getCarrito());
+        factura.setSubtotalSinDescuentos(subtotalSinDescuentos);
+        if (factura.getDescuento() != null) {
+            factura.setSubtotalDescuentos(this.servicioDescuento.calcularDescuento(factura.getDescuento(), factura.getSubtotalSinDescuentos()));
+            factura.setImporteFinal(this.servicioDescuento.calcularImporteConDescuento(factura.getDescuento(), factura.getSubtotalSinDescuentos()));
+        } else {
+            factura.setSubtotalDescuentos(new BigDecimal("0"));
+            factura.setImporteFinal(subtotalSinDescuentos);
+        }
+        this.servicioItem.fijarPrecioDeCompra(this.servicioItem.listarItems(factura.getCarrito()));
+        factura.setFechaYHoraDeCompra(LocalDateTime.now());
+        this.servicioFacturaDao.actualizarFactura(factura);
+        return factura;
+    }
 
-	@Override
-	public Factura obtenerFactura(Long idFactura) {
-		return this.servicioFacturaDao.obtenerFactura(idFactura);
-	}
+    @Override
+    public Factura sincronizarFactura(Long idCarrito) {
+        Carrito carrito = this.servicioCarrito.obtenerCarrito(idCarrito);
+        Factura factura = this.servicioFacturaDao.buscarFacturaSinConfirmar(carrito.getId());
+        if (factura != null) {
+            factura = this.actualizarImportesFactura(factura);
+            return factura;
+        } else {
+            return this.generarFactura(carrito);
+        }
+    }
 
-	@Override
-	public String obtenerMailDelComprador(Factura factura) {
-		return factura.getCarrito().getUsuario().getEmail();
-	}
+    @Override
+    public List<Item> listarItems(Factura factura) {
+        return this.servicioItem.listarItems(factura.getCarrito());
+    }
 
-	@Override
-	public String obtenerNombreDeLaTienda(Factura factura) {
-		return factura.getCarrito().getTienda().getRazonSocial();
-	}
+    @Override
+    public Factura obtenerFactura(Long idFactura) {
+        return this.servicioFacturaDao.obtenerFactura(idFactura);
+    }
 
-	@Override
-	public BigDecimal obtenerImporteFinal(Factura factura) {
-		return factura.getImporteFinal();
-	}
+    @Override
+    public String obtenerMailDelComprador(Factura factura) {
+        return factura.getCarrito().getUsuario().getEmail();
+    }
 
-	@Override
-	public Factura obtenerFacturaPorIdMercadoPago(String preferenceId) {
-		return this.servicioFacturaDao.obtenerFacturaPorIdMercadoPago(preferenceId);
-	}
+    @Override
+    public String obtenerNombreDeLaTienda(Factura factura) {
+        return factura.getCarrito().getTienda().getRazonSocial();
+    }
 
-	@Override
-	public void actualizarFactura(Factura factura) {
-		this.servicioFacturaDao.actualizarFactura(factura);
-		return;
-	}
-	
-	@Override
-	public void pagoExitoso(Factura factura) {
-		factura.setEstado(EnumEstadoDeCompra.COMPRA_FINALIZADA);
-		factura.getCarrito().setEstado(EnumEstadoDeCompra.COMPRA_FINALIZADA);
-		this.servicioFacturaDao.actualizarFactura(factura);
-		this.servicioItem.actualizarStockDeProductos(this.servicioItem.listarItems(factura.getCarrito()));
-	}
+    @Override
+    public BigDecimal obtenerImporteFinal(Factura factura) {
+        return factura.getImporteFinal();
+    }
 
-	@Override
-	public void pagoPendiente(Factura factura) {
-		factura.setEstado(EnumEstadoDeCompra.PENDIENTE_DE_PAGO);
-		factura.getCarrito().setEstado(EnumEstadoDeCompra.PENDIENTE_DE_PAGO);
-		this.servicioFacturaDao.actualizarFactura(factura);
-		this.servicioItem.actualizarStockDeProductos(this.servicioItem.listarItems(factura.getCarrito()));
-	}
+    @Override
+    public Factura obtenerFacturaPorIdMercadoPago(String preferenceId) {
+        return this.servicioFacturaDao.obtenerFacturaPorIdMercadoPago(preferenceId);
+    }
 
-	@Override
-	public boolean checkearStock(Factura factura) {
-		List<Item> items = this.servicioItem.listarItems(factura.getCarrito());
-		for(Item item: items) {
-			if(item.getCantidad() > item.getProducto().getStock()) {
-				return false;
-			}
-		}
-		return true;
-	}
+    @Override
+    public void actualizarFactura(Factura factura) {
+        this.servicioFacturaDao.actualizarFactura(factura);
+        return;
+    }
+
+    @Override
+    public void pagoExitoso(Factura factura) {
+        factura.setEstado(EnumEstadoDeCompra.COMPRA_FINALIZADA);
+        factura.getCarrito().setEstado(EnumEstadoDeCompra.COMPRA_FINALIZADA);
+        this.servicioFacturaDao.actualizarFactura(factura);
+        this.servicioItem.actualizarStockDeProductos(this.servicioItem.listarItems(factura.getCarrito()));
+    }
+
+    @Override
+    public void pagoPendiente(Factura factura) {
+        factura.setEstado(EnumEstadoDeCompra.PENDIENTE_DE_PAGO);
+        factura.getCarrito().setEstado(EnumEstadoDeCompra.PENDIENTE_DE_PAGO);
+        this.servicioFacturaDao.actualizarFactura(factura);
+        this.servicioItem.actualizarStockDeProductos(this.servicioItem.listarItems(factura.getCarrito()));
+    }
+
+    @Override
+    public boolean checkearStock(Factura factura) {
+        List<Item> items = this.servicioItem.listarItems(factura.getCarrito());
+        for (Item item : items) {
+            if (item.getCantidad() > item.getProducto().getStock()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioFacturaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioFacturaTest.java
@@ -1,6 +1,7 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
 import ar.edu.unlam.tallerweb1.modelo.Carrito;
+import ar.edu.unlam.tallerweb1.modelo.Descuento;
 import ar.edu.unlam.tallerweb1.modelo.EnumEstadoDeCompra;
 import ar.edu.unlam.tallerweb1.modelo.Factura;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioFactura;
@@ -9,18 +10,20 @@ import org.junit.Test;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ServicioFacturaTest {
 
+    private RepositorioFactura repositorioFactura = mock(RepositorioFactura.class);
+    private ServicioItem servicioItem = mock(ServicioItem.class);
+    private ServicioCarrito servicioCarrito = mock(ServicioCarrito.class);
+    private ServicioDescuento servicioDescuento = mock(ServicioDescuento.class);
+
     @Test
     public void testFactura() {
 
-        RepositorioFactura repositorioFactura = mock(RepositorioFactura.class);
-        ServicioItem servicioItem = mock(ServicioItem.class);
-        ServicioCarrito servicioCarrito = mock(ServicioCarrito.class);
-        ServicioDescuento servicioDescuento = mock(ServicioDescuento.class);
 
         Carrito mockCarrito = getMockCarrito();
 
@@ -37,6 +40,35 @@ public class ServicioFacturaTest {
         assertThat(resultado.getEstado()).isEqualTo(EnumEstadoDeCompra.SIN_CONFIRMAR);
         assertThat(resultado.getSubtotalSinDescuentos()).isEqualTo(BigDecimal.valueOf(12000L));
 
+    }
+
+    @Test
+    public void testFacturaSinDescuentoTieneComoImporteFinalLaSumaDelImporteDeItemsEnCarrito() {
+        when(servicioItem.sumarImportesDeItems(any())).thenReturn(BigDecimal.valueOf(10000));
+
+        Factura factura = new Factura();
+
+        ServicioFactura servicioFactura = new ServicioFacturaImpl(repositorioFactura, servicioItem, servicioCarrito, servicioDescuento);
+
+        Factura result = servicioFactura.actualizarImportesFactura(factura);
+
+        assertThat(result.getImporteFinal()).isEqualTo(BigDecimal.valueOf(10000));
+    }
+
+    @Test
+    public void testCargarCodigoDescuento() {
+        Descuento descuento = new Descuento();
+        descuento.setPorcentajeDeDescuento(20);
+        descuento.setTope(BigDecimal.valueOf(2000));
+        when(servicioDescuento.verificarValidez(any())).thenReturn(descuento);
+
+        Factura factura = new Factura();
+
+        ServicioFactura servicioFactura = new ServicioFacturaImpl(repositorioFactura, servicioItem, servicioCarrito, servicioDescuento);
+
+        Boolean result = servicioFactura.cargarCodigoDeDescuento(factura, "TEST");
+
+        assertThat(result).isTrue();
     }
 
     private Carrito getMockCarrito() {
